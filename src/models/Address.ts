f5f0f9a25f2a8e8d63dd84fd23fbfa7f42479174
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-05 16:41:01 -0300

    Address model

---
+++ src/models/Address.ts
@@ -0,0 +1,13 @@
+export interface Address {
+  label: string;
+  countryCode: string;
+  countryName: string;
+  state: string;
+  countyCode: string;
+  county: string;
+  city: string;
+  district: string;
+  street: string;
+  postalCode: string;
+  houseNumber: string;
+}
