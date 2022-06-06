a2e3c9ca82fed1bc6b4d88bdc83c9069ed35dbe3
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-06 17:56:25 -0300

    WeatherOverview with data

--- src/screens/WeatherScreen/components/WeatherOverview/WeatherOverview.tsx
+++ src/screens/WeatherScreen/components/WeatherOverview/WeatherOverview.tsx
@@ -3,27 +3,44 @@ import {Image} from 'react-native';
 
 import {Box} from '@components/atoms/Box';
 import {Heading, Paragraph} from '@components/atoms/Text';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from 'styled-components/native';
 
-export function WeatherOverview() {
+import {weatherService} from '@services/weatherService/weatherService';
+
+import {Weather} from 'src/api/weather/WeatherAPIModels';
+import {BoxCard} from '@components/molecules/BoxCard/BoxCard';
+
+interface Props {
+  city: string;
+  weather: Weather;
+  temp: number;
+}
+export function WeatherOverview({city, weather, temp}: Props) {
+  const {colors} = useTheme();
   return (
-    <Box
-      borderWidth={1}
-      borderColor={'primary'}
-      borderRadius={6}
-      paddingX={4}
+    <BoxCard
       flexDirection="row"
       justifyContent="space-between"
       alignItems="center">
       <Image
         style={{width: 200, height: 200, margin: -32}}
-        source={{
-          uri: 'https://openweathermap.org/img/wn/02d@4x.png',
-        }}
+        source={{uri: weatherService.getIconURL(weather.icon)}}
       />
       <Box alignItems={'flex-end'}>
-        <Heading>21</Heading>
-        <Paragraph size="large">city name</Paragraph>
+        <Heading>{Math.round(temp)}°</Heading>
+
+        <Box mt={4} flexDirection="row" alignItems="center">
+          <Icon
+            name="map-marker-outline"
+            size={20}
+            color={colors.onBackground}
+          />
+          <Paragraph ml={2} size="large">
+            {city}
+          </Paragraph>
+        </Box>
       </Box>
-    </Box>
+    </BoxCard>
   );
 }
