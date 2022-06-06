43066615d643986698525388f3cb83c22bf52925
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-06 18:11:56 -0300

    ScreenTemplate canGoBack

--- src/components/templates/screen/ScreenTemplate.tsx
+++ src/components/templates/screen/ScreenTemplate.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-import {Box, BoxProps} from '@components/atoms/Box';
+import {Box, BoxProps, PressableBox} from '@components/atoms/Box';
+import {useNavigation} from '@react-navigation/native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from 'styled-components';
 
-type Props = BoxProps;
-export const ScreenTemplate: React.FC<Props> = ({children, ...rest}) => {
+type Props = {canGoBack?: boolean} & BoxProps;
+export const ScreenTemplate: React.FC<Props> = ({
+  children,
+  canGoBack,
+  ...rest
+}) => {
+  const navigation = useNavigation();
+  const {colors} = useTheme();
   const {top} = useSafeAreaInsets();
 
   return (
@@ -15,6 +24,11 @@ export const ScreenTemplate: React.FC<Props> = ({children, ...rest}) => {
       paddingTop={top + 2}
       paddingBottom={6}
       {...rest}>
+      {canGoBack && (
+        <PressableBox mb={4} onPress={navigation.goBack}>
+          <Icon name={'arrow-left'} size={30} color={colors.onBackground} />
+        </PressableBox>
+      )}
       {children}
     </Box>
   );
