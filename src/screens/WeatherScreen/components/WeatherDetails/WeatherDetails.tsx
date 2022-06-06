97b661544ee55aea2a3ba472401ef463694e5a69
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-06 17:56:40 -0300

    WeatherDetails

---
+++ src/screens/WeatherScreen/components/WeatherDetails/WeatherDetails.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Dimensions, FlatList, ListRenderItemInfo} from 'react-native';
+
+import {Box} from '@components/atoms/Box';
+import {Paragraph} from '@components/atoms/Text';
+import {BoxCard} from '@components/molecules/BoxCard/BoxCard';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from 'styled-components/native';
+
+interface WeatherDetailsProps {
+  humidity: number;
+  pressure: number;
+  uvi: number;
+  wind_speed: number;
+  visibility: number;
+  clouds: number;
+}
+
+const NUM_COLUMNS = 3;
+export function WeatherDetails({
+  humidity,
+  pressure,
+  uvi,
+  wind_speed,
+  visibility,
+  clouds,
+}: WeatherDetailsProps) {
+  const list: ItemProps[] = [
+    {iconName: 'water-percent', value: humidity, label: 'umidade'},
+    {iconName: 'water-percent', value: pressure, label: 'pressão'},
+    {iconName: 'water-percent', value: uvi, label: 'UV'},
+    {iconName: 'water-percent', value: wind_speed, label: 'vento'},
+    {iconName: 'water-percent', value: visibility, label: 'visibilidade'},
+    {iconName: 'water-percent', value: clouds, label: 'nuvens'},
+  ];
+
+  console.log({list});
+
+  function renderItem({item}: ListRenderItemInfo<ItemProps>) {
+    return <Item {...item} />;
+  }
+  return (
+    <BoxCard padding={0} mt={8}>
+      <FlatList
+        numColumns={NUM_COLUMNS}
+        keyExtractor={item => item.label}
+        renderItem={renderItem}
+        data={list}
+      />
+    </BoxCard>
+  );
+}
+
+interface ItemProps {
+  iconName: string;
+  value: number;
+  label: string;
+}
+function Item({iconName, value, label}: ItemProps) {
+  const {colors, space} = useTheme();
+  const WIDTH = (Dimensions.get('screen').width - 2 * space[4]) / NUM_COLUMNS;
+  return (
+    <Box width={WIDTH} alignItems="center" padding={4}>
+      <Icon name={iconName} size={30} color={colors.onBackground} />
+      <Paragraph fontWeight="bold">{value}</Paragraph>
+      <Paragraph>{label}</Paragraph>
+    </Box>
+  );
+}
