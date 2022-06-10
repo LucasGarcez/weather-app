fa52b576ba107fab903ffb58b91be71b128b8c97
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-10 11:34:50 -0300

    add react query

--- App.tsx
+++ App.tsx
@@ -3,19 +3,23 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {ThemeProvider} from 'styled-components/native';
-
-Icon.loadFont();
 
 import {darkTheme} from 'src/themes/darkTheme';
 
 import {Router} from './src/router/Router';
 
+Icon.loadFont();
+
+const queryClient = new QueryClient();
 const App = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
