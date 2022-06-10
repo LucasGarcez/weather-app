f07d0c0c53bfa3623055eae7d9c246bafd50ba9f
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-10 11:35:36 -0300

    AddressInfo

---
+++ src/screens/AddressScreen/components/AddressInfo/AddressInfo.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import {Button} from '@components/atoms/Button/Button';
+import {LoadingIndicator} from '@components/molecules/LoadingIndicator/LoadingIndicator';
+import {useNavigation} from '@react-navigation/native';
+import {useQuery} from 'react-query';
+
+import {geolocationService} from '@services/geolocationService/geolocationService';
+
+import {GeoCoordinates} from 'src/models/Geolocation';
+
+import {InfoList} from '../InfoList/InfoList';
+
+interface Props {
+  geoCoordinates: GeoCoordinates;
+}
+export function AddressInfo({geoCoordinates}: Props) {
+  const navigation = useNavigation();
+
+  const {
+    data: address,
+    isLoading,
+    error,
+  } = useQuery('address', () =>
+    geolocationService.getAddressByCoordinates(
+      geoCoordinates.latitude,
+      geoCoordinates.longitude,
+    ),
+  );
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) {
+    //handle erro
+  }
+
+  if (address) {
+    return (
+      <>
+        <InfoList
+          title="Endereço"
+          items={[
+            {label: 'País', value: address.countryName},
+            {label: 'Estado', value: address.state},
+            {label: 'Cidade', value: address.city},
+            {label: 'Rua', value: address.street},
+            {label: 'Número', value: address.houseNumber},
+          ]}
+        />
+
+        <Button
+          mt={8}
+          variant="secondary"
+          iconName="weather-sunny"
+          title="VER CLIMA"
+          onPress={() =>
+            navigation.navigate('WeatherScreen', {
+              geoCoordinates: geoCoordinates,
+              address,
+            })
+          }
+        />
+      </>
+    );
+  }
+
+  return null;
+}
