f4bc3839b96ff306570f654e4a20b76cb2ae9e00
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-10 11:35:29 -0300

    CoordinatesInfo

---
+++ src/screens/AddressScreen/components/CoordinatesInfo/CoordinatesInfo.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {LoadingIndicator} from '@components/molecules/LoadingIndicator/LoadingIndicator';
+import {useQuery} from 'react-query';
+
+import {geolocationService} from '@services/geolocationService/geolocationService';
+
+import {PermissionError} from 'src/models/error/PermissionError';
+
+import {InfoList} from '../InfoList/InfoList';
+import {PermissionBox} from '../PermissionBox/PermissionBox';
+
+export function CoordinatesInfo() {
+  const {data, error, isLoading} = useQuery(
+    'geolocation',
+    geolocationService.getCurrentCoordinates,
+  );
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) {
+    if (error instanceof PermissionError) {
+      return <PermissionBox status={error.status} />;
+    }
+  }
+
+  if (data) {
+    return (
+      <InfoList
+        title="Coordenadas"
+        items={[
+          {label: 'Latitude', value: data.latitude},
+          {label: 'Longitude', value: data.latitude},
+        ]}
+      />
+    );
+  }
+
+  return null;
+}
