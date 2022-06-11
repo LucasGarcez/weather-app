081a3e34b5c394889e7201ae96ecec1b8ee4d283
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-06-11 10:43:38 -0300

    SelectWeekDay initialScrollIndex

--- src/components/molecules/SelectWeekDay/SelectWeekDay.tsx
+++ src/components/molecules/SelectWeekDay/SelectWeekDay.tsx
@@ -10,6 +10,8 @@ import {DaysOfWeek} from 'src/models/DaysOfWeek';
 import {Day, DAYS} from './DAYS';
 import {DayBox} from './styles';
 
+const DAY_BOX_WIDTH = 70;
+
 interface Props {
   value: DaysOfWeek;
   onChangeValue: (day: DaysOfWeek) => void;
@@ -18,6 +20,7 @@ export function SelectWeekDay({value, onChangeValue}: Props) {
   function renderItem({item}: ListRenderItemInfo<Day>) {
     return (
       <DayBox
+        width={DAY_BOX_WIDTH}
         key={item.value}
         onPress={() => onChangeValue(item.value)}
         isSelected={value === item.value}>
@@ -29,6 +32,10 @@ export function SelectWeekDay({value, onChangeValue}: Props) {
   return (
     <Box>
       <FlatList
+        getItemLayout={(_, index) => {
+          return {length: DAY_BOX_WIDTH, offset: DAY_BOX_WIDTH, index};
+        }}
+        initialScrollIndex={DAYS.findIndex(day => day.value === value)}
         data={DAYS}
         renderItem={renderItem}
         horizontal
